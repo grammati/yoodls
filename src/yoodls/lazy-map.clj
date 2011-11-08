e58ca8af5500db9804e9f7fe178f8e005dde4f9b
Chris Perkins <chrisperkins99@gmail.com>
2011-11-08 07:08:20 -0500

    lazy-map somewhat working

--- src/yoodls/lazy-map.clj
+++ src/yoodls/lazy-map.clj
@@ -1,26 +1,42 @@
 (ns yoolds.lazy-map)
 
 
+(defn delay-map
+  ([]
+     (delay-map nil))
+  ([m]
+     (letfn [(value-of [v]
+               (if (instance? clojure.lang.IDeref v) @v v))]
+       (proxy [clojure.lang.APersistentMap] []
+         (assoc [k v]
+           (delay-map (assoc m k v)))
+         (without [k]
+           (delay-map (dissoc m k)))
+         (valAt
+           ([k]
+              (.valAt this k nil))
+           ([k not-found]
+              (value-of (get m k not-found))))
+         (seq []
+           ;(for [[k v] (seq m)] [k (value-of v)]) ;; ???
+           (seq m))
+         (count []
+           (count m))
+         ))))
 
-#_(defn delay-map [m]
-  (proxy [clojure.lang.APersistentMap] []
-    (assoc [k v] (delay-map (assoc m k v)))
-    (without [k] (delay-map (dissoc m k)))
-    (valAt [k not-found]
-      (let [v (get m k not-found)]
-        (if (instance? clojure.lang.IDeref v) @v v)))
-    (valAt [k]
-      (.valAt this k nil))))
 
 
-(defn delay-map [m]
-  (reify
-    clojure.lang.IPersistentMap
-    (assoc [this k v] (delay-map (assoc m k v)))
-    (without [this k] (delay-map (dissoc m k)))
-    (valAt [this k not-found]
-      (let [v (get m k not-found)]
-        (if (instance? clojure.lang.IDeref v) @v v)))
-    (valAt [this k]
-      (.valAt this k nil))))
+(defmacro lazy-map [& keyvals]
+  (when (odd? (count keyvals))
+    (throw (Exception. "lazy-map requires an even number of forms")))
+  (list
+   `delay-map
+   (into {}
+         (for [[k v] (partition 2 keyvals)]
+           [k (if (and (list? v) (symbol? (first v)))
+                (list `delay v)
+                v)]))))
 
+(def test (lazy-map
+           :foo (do (println "evaluated!") :bar)
+           :xxx "yyy"))
