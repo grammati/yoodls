520950423c8bb9ac1777da6508b545919c5ead73
Chris Perkins <chrisperkins99@gmail.com>
2011-11-16 18:05:45 -0500

    delay-map - it works now

--- src/yoodls/lazy-map.clj
+++ src/yoodls/lazy-map.clj
@@ -2,27 +2,31 @@
 
 
 (defn delay-map
-  ([]
-     (delay-map nil))
-  ([m]
-     (letfn [(value-of [v]
-               (if (instance? clojure.lang.IDeref v) @v v))]
-       (proxy [clojure.lang.APersistentMap] []
-         (assoc [k v]
-           (delay-map (assoc m k v)))
-         (without [k]
-           (delay-map (dissoc m k)))
-         (valAt
-           ([k]
-              (.valAt this k nil))
-           ([k not-found]
-              (value-of (get m k not-found))))
-         (seq []
-           ;(for [[k v] (seq m)] [k (value-of v)]) ;; ???
-           (seq m))
-         (count []
-           (count m))
-         ))))
+  [& [m]]
+  (proxy [clojure.lang.APersistentMap] []
+    ;; This method implements the "real" funtionality:
+    (valAt
+      ([k] (force (get m k)))
+      ([k not-found] (force (get m k not-found))))
+
+    ;; These methods are to preserve the type by returning
+    ;; new delay-map instances when required.
+    (assoc [k v]
+      (delay-map (assoc m k v)))
+    ;;(assocEx [k v] ???)
+    (without [k]
+      (delay-map (dissoc m k)))
+    (withMeta [md]
+      (delay-map (with-meta m md)))
+
+    ;; boilerplate - just delegate:
+    (containsKey [k] (contains? m k))
+    (count [] (count m))
+    (entryAt [k] (.entryAt ^clojure.lang.Associative m))
+    (iterator [] (.iterator ^java.util.Map m))
+    (meta [] (meta m))
+    (seq [] (seq m))
+    ))
 
 
 
@@ -32,10 +36,14 @@
   (list
    `delay-map
    (into {}
-         (for [[k v] (partition 2 keyvals)]
-           [k (if (and (list? v) (symbol? (first v)))
-                (list `delay v)
-                v)]))))
+     (for [[k v] (partition 2 keyvals)]
+       [k (if (and (list? v) (symbol? (first v)))
+            (list `delay v)
+            v)]))))
+
+(defmacro lazy-assoc [m & keyvals]
+  `(delay-map (merge ~m (lazy-map ~@keyvals))))
+
 
 (def test (lazy-map
            :foo (do (println "evaluated!") :bar)
