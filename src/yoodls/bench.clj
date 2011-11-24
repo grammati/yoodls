2fc0cf18617f2d9fc1df54aa0f1e3f1961ed3a67
Chris Perkins <chrisperkins99@gmail.com>
2011-11-24 15:06:16 -0500

    updates for benchmarking, and experimenting with lazy-map

--- src/yoodls/bench.clj
+++ src/yoodls/bench.clj
@@ -1,27 +1,45 @@
 (ns yoodls.bench)
 
-(defn bench-fn [f]
-  (let [time-f (fn [n]
+(defn bench-fn
+  [f]
+  (let [noop   (fn [] nil)
+        time-n-reps
+               (fn [f n]
                  (let [start (System/nanoTime)]
                    (dotimes [_ n]
                      (f))
                    (- (System/nanoTime) start)))
+        timer  (fn [f n]
+                 (let [t (- (time-n-reps f n)
+                            (time-n-reps noop n))]
+                   (if (neg? t) 0 t)))
         devnul (proxy [java.io.Writer] []
-                 (write [])
+                 (write [_])
                  (flush []))]
-                                        ;(binding [*out* devnul])
-    (loop [n 1]
-      (let [t (time-f n)]
-        (if (< t 1e8)
-          (recur (* 2 n))
-          (double ( / t n)))))))
+    (binding [*out* devnul]
+      (System/gc)
+      (let [rep-count (loop [n 1]
+                        (let [t (timer f n)]
+                          (if (or (> t 1e8) (> n 1e9))
+                            n
+                            (recur (* 2 n)))))
+            timeit (fn []
+                     (/ (double (timer f rep-count)) rep-count))]
+        ;; warm-up the JIT
+        (dotimes [_ 5] (f))
+        (System/gc) 
+        (first (sort (take 3 (repeatedly timeit))))))))
 
-(defmacro bench [& body]
-  `(let [ns# (bench-fn (fn [] ~@body))
-         [n# u#] (cond
-                  (> ns# 1e9) [(/ ns# 1e9) "s"]
-                  (> ns# 1e6) [(/ ns# 1e6) "ms"]
-                  (> ns# 1e3) [(/ ns# 1e3) "us"]
-                  :else       [ns# "ns"])]
-     (println (format "%g %s" n# u#))))
+(defn nanos-to-string [nanos]
+  (let [[n unit] (cond
+                  (> nanos 1e9) [(/ nanos 1e9) "s"]
+                  (> nanos 1e6) [(/ nanos 1e6) "ms"]
+                  (> nanos 1e3) [(/ nanos 1e3) "us"]
+                  :else       [nanos "ns"])]
+    (format "%g %s" n unit)))
+
+(defmacro bench
+  "Measure and print the execution time of the body."
+  [& body]
+  `(println (nanos-to-string (bench-fn (fn [] ~@body)))))
 
--- src/yoodls/lazy-map.clj
+++ src/yoodls/lazy-map.clj
@@ -1,7 +1,20 @@
 (ns yoolds.lazy-map)
 
 
-(defn delay-map
+(defprotocol LazyMapValue
+  (force-value [this] "Applied to each value extracted from a lazy map."))
+
+(extend-protocol LazyMapValue
+  Object
+  (force-value [this] this)
+  clojure.lang.Delay
+  (force-value [this] (deref this))
+  java.util.concurrent.Future
+  (force-value [this] (deref this))
+  )
+
+
+(defn lazy-map*
   "Wraps the given map such that any value that is a Delay will be
   forced when extracted."
   [& [m]]
@@ -11,46 +24,72 @@
     
     ;; This method implements the "real" funtionality:
     (valAt
-      ([k] (force (get m k)))
-      ([k not-found] (force (get m k not-found))))
+      ([k] (force-value (get m k)))
+      ([k not-found] (force-value (get m k not-found))))
 
     ;; These methods are to preserve the type by returning
-    ;; new delay-map instances when required.
+    ;; new lazy-map* instances when required.
     (assoc [k v]
-      (delay-map (assoc m k v)))
+      (lazy-map* (assoc m k v)))
     ;;(assocEx [k v] ???)
     (without [k]
-      (delay-map (dissoc m k)))
+      (lazy-map* (dissoc m k)))
     (withMeta [md]
-      (delay-map (with-meta m md)))
+      (lazy-map* (with-meta m md)))
 
     ;; boilerplate - just delegate:
     (containsKey [k] (contains? m k))
     (count [] (count m))
-    (entryAt [k] (.entryAt ^clojure.lang.Associative m))
+    (entryAt [k] (.entryAt ^clojure.lang.Associative m k))
     (iterator [] (.iterator ^java.util.Map m))
     (meta [] (meta m))
     (seq [] (seq m))
+    ;(seq [] (for [[k v] m] (clojure.lang.MapEntry. k (force-value v))))
 
     (asTransient []
       (throw (Exception. "I don't know how to implement this yet.")))
     ))
 
 
+(defn- lazify
+  [form]
+  (if (and (list? form)
+           (symbol? (first form))
+           (not (#{'future 'delay} (first form))))
+    (list `delay form)
+    form))
 
-(defmacro lazy-map [& keyvals]
-  (when (odd? (count keyvals))
-    (throw (Exception. "lazy-map requires an even number of forms")))
-  (list
-   `delay-map
-   (into {}
-     (for [[k v] (partition 2 keyvals)]
-       [k (if (and (list? v) (symbol? (first v)))
-            (list `delay v)
-            v)]))))
+(defmacro lazy-map
+  "Creates a lazy-map from an alternating list of keys and values.
+   A lazy map is equivalent to a normal clojure map except in 2 cases:
+   1) When a value is a delay, it is evaluated only when first accessed.
+   2) When a value is a future, it is dereferenced when accessed.
 
-(defmacro lazy-assoc [m & keyvals]
-  `(delay-map (merge ~m (lazy-map ~@keyvals))))
+   This macro helps with the creation the lazy map by wrapping any value
+   that appears to be a function call in a delay, unless it appears to
+   be a future.
+
+   Example:
+   (lazy-map :v \"normal value\"
+             :d (do (println \"delay evaluated\") :delay)
+             :f (future (Thread/sleep 2000) :future))
+
+   :d's value will be evaluated only if accessed.
+   :f's value will be calculated in a thread, and accessing it could block.
+  "
+  [& kvs]
+  (when (odd? (count kvs))
+    (throw (IllegalArgumentException. "lazy-map requires an even number of forms")))
+  (list `lazy-map*
+        (reduce (fn [m [k v]]
+                  (.assocEx m k (lazify v)))
+                {}
+                (partition 2 kvs))))
+
+(defmacro lazy-assoc
+  ""
+  [m & keyvals]
+  `(lazy-map* (merge ~m (lazy-map ~@keyvals))))
 
 
 (comment
